3b24c24eadf38e24406f667b24bf9fe3a5ffd06d
Petr Leontev <raianknight@gmail.com>
2024-04-22 17:21:21 +0100

    Remove redundant fields

--- client/js/state/Player.ts
+++ client/js/state/Player.ts
@@ -1,7 +1,5 @@
 export class Player {
     public playerId: string;
-    public userId: string;
-    public name: string;
     
     constructor(playerId: string) {
         this.playerId = playerId;
