8e150c871d5c07f7cfc4e5e7c4450fa00efc9e8e
Petr Leontev <raianknight@gmail.com>
2024-05-01 21:49:26 +0100

    Fix packaging

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,14 @@
 import { DiscordSDK } from "@discord/embedded-app-sdk";
-import { Boot } from './src/scenes/Boot'
-import { GameOver } from './src/scenes/GameOver';
-import { Preloader } from './src/scenes/Preloader';
-import { MainMenu } from './src/scenes/MainMenu';
-import { TactonGame } from './src/scenes/Game';
+import { Boot } from './scenes/Boot'
+import { GameOver } from './scenes/GameOver';
+import { Preloader } from './scenes/Preloader';
+import { MainMenu } from './scenes/MainMenu';
+import { TactonGame } from './scenes/Game';
 
 
 // Instantiate the SDK
 // Uncomment this once your work is done on browser, it will ONLY work on Discord Activities
-const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+/*const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID, {disableConsoleLogOverride: true});
 setupDiscordSdk().then(() => {
   console.log("Discord SDK is ready");
 }).catch((error) => {
@@ -16,7 +16,7 @@ setupDiscordSdk().then(() => {
 });
 async function setupDiscordSdk() {
   await discordSdk.ready();
-}
+}*/
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
