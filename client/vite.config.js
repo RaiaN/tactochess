774f090e716e6fa8a92d44c959f1d218c71d1b20
Petr Leontev <raianknight@gmail.com>
2024-05-01 23:49:37 +0100

    Fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '../',
   server: {
     /**
        * When developing locally - proxies "/api" to the local Colyseus server.
